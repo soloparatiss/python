5735848f7fdbc4939dea07c389d6d8d22c2dd48b
Carlos I <soloparatiss@hotmail.com>
2014-09-02 12:36:51 -0500

    Cambiando aspecto draw

--- mindstorms/mindstorms.py
+++ mindstorms/mindstorms.py
@@ -5,6 +5,11 @@ import turtle
 def draw_square():
 	window = turtle.Screen()
 	window.bgcolor("red")
+	
+	brad = turtle.Turtle()
+	brad.shape("turtle")
+	brad.color("yellow")
+	brad.speed(2)
 
 	brad = turtle.Turtle()
 	brad.forward(100)

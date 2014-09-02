360cc2f70dae0f569fe6e0773f4274e107a75862
Carlos I <soloparatiss@hotmail.com>
2014-09-02 11:20:11 -0500

    Una vez se tiene lista archivos, se procede a renombrar uno a uno quitando los numeros del nombre del archivo

--- renameFiles/rename_files.py
+++ renameFiles/rename_files.py
@@ -6,4 +6,7 @@ def rename_files():
 	file_list = os.listdir(r"/home/usuario/Desktop/workspacepython")
 	print (file_list)
 
+	for file_name in file_list:
+		os.rename(file_name,file_name.translate(None,"0123456789"))
+
 rename_files()

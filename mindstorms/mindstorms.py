57571598af65a014932daf145ab8edfabe76ff12
Carlos I <soloparatiss@hotmail.com>
2014-09-02 14:05:30 -0500

    Refactoring software

--- mindstorms/mindstorms.py
+++ mindstorms/mindstorms.py
@@ -2,7 +2,12 @@
 
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
+	for i in range(1,5):
+		some_turtle.forward(100)
+		some_turtle.right(90)
+
+def draw_art():
 	window = turtle.Screen()
 	window.bgcolor("red")
 	
@@ -12,14 +17,10 @@ def draw_square():
 	brad.speed(2)
 
 	brad = turtle.Turtle()
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
+	brad.shape("turtle")
+	brad.color("yellow")
+	brad.speed(2)
+	draw_square(brad)
 
 	angie = turtle.Turtle()
 	angie.shape("arrow")

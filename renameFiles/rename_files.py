7797c52b3264235e9df57bad92f00bcacddaca18
Carlos I <soloparatiss@hotmail.com>
2014-09-02 11:29:59 -0500

    Definir directorio de busqueda

--- renameFiles/rename_files.py
+++ renameFiles/rename_files.py
@@ -5,8 +5,12 @@ import os
 def rename_files():
 	file_list = os.listdir(r"/home/usuario/Desktop/workspacepython")
 	print (file_list)
+	saved_path = os.getcwd()
+	os.chdir(r"/home/usuario/Desktop/workspacepython")
 
 	for file_name in file_list:
 		os.rename(file_name,file_name.translate(None,"0123456789"))
+	
+	os.chdir(save_path)
 
 rename_files()
